f10b139d82b4dd22ebca08775097cb736a53d8c2
Travis CI <>
2015-05-24 10:13:17 +0000

    Updated the documentation

---
+++ probability/distributions/sidebar-items.js
@@ -0,0 +1,1 @@
+initSidebarItems({"struct":[["Beta","A beta distribution."],["Exponential","An exponential distribution."],["Gaussian","A Gaussian distribution."],["Uniform","A continuous uniform distribution."]]});
